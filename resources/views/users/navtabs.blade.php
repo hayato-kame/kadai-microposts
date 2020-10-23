8fe4668cf486457c7d0138a655954aa0f3f1a738
Ubuntu <ubuntu@ip-172-31-24-110.ap-northeast-1.compute.internal>
2020-10-23 17:14:39 +0000

    follow, unfollow

---
+++ resources/views/users/navtabs.blade.php
@@ -0,0 +1,30 @@
+<ul class="nav nav-tabs nav-justified mb-3">
+    
+    <li class="nav-item">
+        <a href="{{ route('users.show', ['user' => $user->id] }}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}"> 
+        TimeLine
+        <span class="badge badge-secondary">
+            {{ $user->microposts_count }}
+        </span>
+        </a>
+    </li>
+    
+    <li class="nav-item">
+        <a href="{{ route('users.followings', ['id' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.followings') ? 'active' : '' }}"></users>"
+        Followings
+        <span class="badge badge-secondary">
+            {{ $user->followings_count }}
+        </span>
+        </a>
+    </li>
+    
+    <li class="nav-item">
+        <a href="{{ route('users.followers', ['id' => $user->id]) }}" class="nav-link {{ Route::routeIs('users.followers') ? 'active' : '' }}">
+            Followers
+            <span>
+                {{ $user->followers_count }}
+            </span>
+        </a>
+    </li>
+    
+</ul>
